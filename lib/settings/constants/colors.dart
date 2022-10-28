49eb49db9052b61907d94048a6d45dd15d0ceb57
Omaralkhanjy94 <45111028+Omaralkhanjy94@users.noreply.github.com>
2022-10-29 00:30:05 +0200

    Add files via upload

---
+++ lib/settings/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  static const Color primaryColor = Color(0xffffffaa);
+  static const Color secondColor = Color(0xff0055aa);
+  static const Color textColor = Color(0xff636ab8);
+
+  static const MaterialColor mainColors = MaterialColor(1, {
+      1: primaryColor,
+      2: secondColor
+  });
+
+}
