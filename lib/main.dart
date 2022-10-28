910b998ddf0bc413b86754b67b004cf78555305a
Omaralkhanjy94 <45111028+Omaralkhanjy94@users.noreply.github.com>
2022-10-29 00:21:39 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:alfabitaka/Settings/Constants/colors.dart';
+import 'package:alfabitaka/Settings/properties.dart';
+import 'package:alfabitaka/screens/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MainRoute();
+  }
+}
