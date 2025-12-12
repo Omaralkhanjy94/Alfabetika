7d9008c39cbdf44eb5a90b70e7df7a4f15647dde
Omaralkhanjy94 <omaralkhanjy@gmail.com>
2025-12-12 21:28:46 +0300

    إرجاع

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-import 'package:alfabitaka/Settings/Constants/colors.dart';
 import 'package:alfabitaka/Settings/properties.dart';
-import 'screens/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Alfabetika());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Alfabetika extends StatelessWidget {
+  const Alfabetika({super.key});
 
   // This widget is the root of your application.
   @override
